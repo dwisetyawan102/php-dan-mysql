8f61704e75471f91ba8b5be3d64317b5552d7054
Dwi Setyawan <82011645+dwisetyawan102@users.noreply.github.com>
2023-11-30 08:55:04 +0700

    refactor kode query kedalam function

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php 
-  // koneksi ke database
-  $conn = mysqli_connect("localhost", "root", "", "phpdasar");
-
-  // query data dari tabel mahasiswa
-  $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
-
-  // mengecek apakah koneksi ke database berhasil atau tidak
-  if( !$result ) {
-    echo mysqli_error($conn);
-  }
-
-  // ambil data (fetch) mahasiswa dari object result
-  // mysqli_fetch_row()
-  // mysqli_fetch_assoc()
-  // mysqli_fetch_array()
-  // mysqli_fetch_object() 
+  require 'functions.php';
+  $mahasiswa = query("SELECT * FROM mahasiswa");
 ?>
 
 <!DOCTYPE html>
@@ -39,20 +25,20 @@
       <th>Jurusan</th>
     </tr>
     <?php $i = 1 ?>
-    <?php while( $row = mysqli_fetch_assoc($result) ) { ?>
-      <tr>
-        <td><?php echo $i++ ?></td>
-        <td>
-          <a href="">Ubah</a> |
-          <a href="">Hapus</a>
-        </td>
-        <td><img src="img/<?php echo $row["gambar"] ?>" alt="" width="50"></td>
-        <td><?php echo $row["nim"] ?></td>
-        <td><?php echo $row["nama"] ?></td>
-        <td><?php echo $row["email"] ?></td>
-        <td><?php echo $row["jurusan"] ?></td>
-      </tr>
-    <?php } ?>
+    <?php foreach( $mahasiswa as $row ) : ?>
+    <tr>
+      <td><?php echo $i++; ?></td>
+      <td>
+        <a href="">Ubah</a> |
+        <a href="">Hapus</a>
+      </td>
+      <td><img src="img/<?php echo $row["gambar"] ?>" alt="" width="50"></td>
+      <td><?php echo $row["nim"] ?></td>
+      <td><?php echo $row["nama"] ?></td>
+      <td><?php echo $row["email"] ?></td>
+      <td><?php echo $row["jurusan"] ?></td>
+    </tr>
+    <?php endforeach; ?>
   </table>
 
 </body>
