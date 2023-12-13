f0d7b5ff7f1c2687e02d1e4ba0820633c9652774
Dwi Setyawan <82011645+dwisetyawan102@users.noreply.github.com>
2023-12-13 10:18:40 +0700

    menambahkan fitur pagination

--- index.php
+++ index.php
@@ -8,7 +8,25 @@
   }
 
   require 'functions.php';
-  $mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id DESC");
+
+  // pagination
+  // konfigurasi
+  $jumlahDataPerHalaman = 2;
+  $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+  $jumlahData = mysqli_num_rows($result);
+  // untuk mencari jumlahData dapat menggunakan cara ini
+  // $jumlahData = count(query("SELECT * FROM mahasiswa"));
+  $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+  if( isset($_GET["halaman"]) ) {
+    $halamanAktif = $_GET["halaman"];
+  } else {
+    $halamanAktif = 1;
+  }
+  // untuk mengetahui / mengisi halamanAktif dapat menggunakan cara ini
+  // $halamanAktif = ( isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1;
+  $awalData = ( $jumlahDataPerHalaman * $halamanAktif ) - $jumlahDataPerHalaman;
+  
+  $mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id DESC LIMIT $awalData, $jumlahDataPerHalaman");
 
   if( isset($_POST["cari"]) ) {
     $mahasiswa = cari($_POST["keyword"]);
@@ -35,6 +53,24 @@
     <input type="text" name="keyword" size="40" autofocus placeholder="masukan keyword pencarian..." autocomplete="off">
     <button type="submit" name="cari">Cari!</button>
   </form>
+  <br><br>
+
+  <?php if( $halamanAktif > 1 ) : ?>
+    <a href="?halaman=<?php echo $halamanAktif - 1 ?>">&laquo;</a>
+  <?php endif; ?>  
+  
+  <?php for($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+    <?php if( $i == $halamanAktif ) : ?>
+      <a href="?halaman=<?php echo $i ?>" style="font-weight: bold; color: red;"><?php echo $i ?></a>
+    <?php else : ?>
+      <a href="?halaman=<?php echo $i ?>"><?php echo $i ?></a>
+    <?php endif; ?>
+  <?php endfor; ?>
+
+  <?php if( $halamanAktif < $jumlahHalaman ) : ?>
+    <a href="?halaman=<?php echo $halamanAktif + 1 ?>">&raquo;</a>
+  <?php endif; ?>
+
   <br>
 
   <table border="1" cellpadding="10" cellspacing="0">
